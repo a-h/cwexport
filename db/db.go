51c0eadad907d6cc4ce402ee0256c697b800a9dc
antonielabuschagne <antonie.labuschagne@infinityworks.com>
2022-03-15 14:52:14 +0000

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ db/db.go
@@ -0,0 +1,14 @@
+package db
+
+func GetRuntime() {
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+}
