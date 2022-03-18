9a999c4b2bcad3a0d36f3a25b013ccc01de86973
Rahul Gupta <grahulx+github@gmail.com>
2022-03-18 11:47:27 +0000

    resolving conflicts

--- db/metric_test.go
+++ db/metric_test.go
@@ -24,21 +24,21 @@ func TestMetricStore(t *testing.T) {
 	t.Run("it cannot get a start time for a metric that doesn't exist", func(t *testing.T) {
 		_, ok, err := ms.Get(ctx, &cw.MetricStat{
 			Metric: &cw.Metric{
+				Namespace:  aws.String("emptyNamespace"),
+				MetricName: aws.String("missingMetric"),
 				Dimensions: []cw.Dimension{
 					{
-						Name:  "ServiceName",
-						Value: "sn",
+						Name:  aws.String("ServiceName"),
+						Value: aws.String("sn"),
 					},
 					{
-						Name:  "ServiceType",
-						Value: "st",
+						Name:  aws.String("ServiceType"),
+						Value: aws.String("st"),
 					},
 				},
-				MetricName: "missingMetric",
-				Namespace:  "emptyNamespace",
 			},
-			Period: aws.Int32(int32((5 * time.Minute).Seconds())),
-			Stat:   stat,
+			Period: aws.Int32(1),
+			Stat:   aws.String("Sum"),
 		})
 		if err != nil {
 			t.Fatalf("unexpected error getting metric: %v", err)
@@ -49,22 +49,46 @@ func TestMetricStore(t *testing.T) {
 	})
 	lastStart := time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC)
 	t.Run("it can insert a metric", func(t *testing.T) {
-		err := ms.Put(ctx, cw.Metric{
-			Namespace:   "ns",
-			Name:        "metricA",
-			ServiceName: "sn",
-			ServiceType: "st",
+		err := ms.Put(ctx, &cw.MetricStat{
+			Metric: &cw.Metric{
+				Namespace:  aws.String("ns"),
+				MetricName: aws.String("metricA"),
+				Dimensions: []cw.Dimension{
+					{
+						Name:  aws.String("ServiceName"),
+						Value: aws.String("sn"),
+					},
+					{
+						Name:  aws.String("ServiceType"),
+						Value: aws.String("st"),
+					},
+				},
+			},
+			Period: aws.Int32(1),
+			Stat:   aws.String("Sum"),
 		}, lastStart)
 		if err != nil {
 			t.Fatalf("unexpected error putting metric: %v", err)
 		}
 	})
 	t.Run("it can get a start time for a previously inserted metric", func(t *testing.T) {
-		actualLastStart, ok, err := ms.Get(ctx, cw.Metric{
-			Namespace:   "ns",
-			Name:        "metricA",
-			ServiceName: "sn",
-			ServiceType: "st",
+		actualLastStart, ok, err := ms.Get(ctx, &cw.MetricStat{
+			Metric: &cw.Metric{
+				Namespace:  aws.String("ns"),
+				MetricName: aws.String("metricA"),
+				Dimensions: []cw.Dimension{
+					{
+						Name:  aws.String("ServiceName"),
+						Value: aws.String("sn"),
+					},
+					{
+						Name:  aws.String("ServiceType"),
+						Value: aws.String("st"),
+					},
+				},
+			},
+			Period: aws.Int32(1),
+			Stat:   aws.String("Sum"),
 		})
 		if err != nil {
 			t.Fatalf("unexpected error getting metric: %v", err)
@@ -78,22 +102,46 @@ func TestMetricStore(t *testing.T) {
 	})
 	lastStart = time.Date(2000, time.January, 2, 0, 0, 0, 0, time.UTC)
 	t.Run("it can update a previously inserted metric", func(t *testing.T) {
-		err := ms.Put(ctx, cw.Metric{
-			Namespace:   "ns",
-			Name:        "metricA",
-			ServiceName: "sn",
-			ServiceType: "st",
+		err := ms.Put(ctx, &cw.MetricStat{
+			Metric: &cw.Metric{
+				Namespace:  aws.String("ns"),
+				MetricName: aws.String("metricA"),
+				Dimensions: []cw.Dimension{
+					{
+						Name:  aws.String("ServiceName"),
+						Value: aws.String("sn"),
+					},
+					{
+						Name:  aws.String("ServiceType"),
+						Value: aws.String("st"),
+					},
+				},
+			},
+			Period: aws.Int32(1),
+			Stat:   aws.String("Sum"),
 		}, lastStart)
 		if err != nil {
 			t.Fatalf("unexpected error putting metric: %v", err)
 		}
 	})
 	t.Run("it can get the start time for an updated metric", func(t *testing.T) {
-		actualLastStart, ok, err := ms.Get(ctx, cw.Metric{
-			Namespace:   "ns",
-			Name:        "metricA",
-			ServiceName: "sn",
-			ServiceType: "st",
+		actualLastStart, ok, err := ms.Get(ctx, &cw.MetricStat{
+			Metric: &cw.Metric{
+				Namespace:  aws.String("ns"),
+				MetricName: aws.String("metricA"),
+				Dimensions: []cw.Dimension{
+					{
+						Name:  aws.String("ServiceName"),
+						Value: aws.String("sn"),
+					},
+					{
+						Name:  aws.String("ServiceType"),
+						Value: aws.String("st"),
+					},
+				},
+			},
+			Period: aws.Int32(1),
+			Stat:   aws.String("Sum"),
 		})
 		if err != nil {
 			t.Fatalf("unexpected error getting metric: %v", err)
